21e875d17cda05f4dcddb0bf0fa716a8d752e405
Dror Arazi <dror.arazi@gmail.com>
2017-10-19 12:46:15 +0300

    changing code again (bug-hunting)

--- gson/src/main/java/com/google/gson/JsonObject.java
+++ gson/src/main/java/com/google/gson/JsonObject.java
@@ -58,7 +58,7 @@ public final class JsonObject extends JsonElement {
     if (value == null) {
       value = JsonNull.INSTANCE;
     }
-    int a = 0;
+    int a = 1;
     members.put(property, value);
   }
 
